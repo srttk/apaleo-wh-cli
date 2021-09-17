47d1d296a00bac9bdf5bf16b157a9fcda0cd20c7
Sarath <sarath.tk@cordiant.com>
2021-09-17 16:42:23 +0530

    Loading indicator

--- src/comands/account.ts
+++ src/comands/account.ts
@@ -1,6 +1,7 @@
 import { program } from 'commander'
 import Table from 'cli-table'
 import { getApaleoInstance } from '../init'
+import { getError, loadingText } from '../utils'
 
 export const accountCommand = program.command("account")
     .description("Get account details")
@@ -8,17 +9,26 @@ export const accountCommand = program.command("account")
 
         const apaleo = getApaleoInstance();
 
-        let account = await apaleo.getAccount()
-        var table = new Table({
-            head: ['Code', 'Name', "Type"],
-            colWidths: [20, 30, 20]
-        });
+        let s = loadingText("Loading account info")
+        try {
 
-        table.push([
-            account.code,
-            account.name,
-            account.type
-        ])
+            let account = await apaleo.getAccount()
+            s.stop();
+            var table = new Table({
+                head: ['Code', 'Name', "Type"],
+                colWidths: [20, 30, 20]
+            });
 
-        console.log(table.toString());
+            table.push([
+                account.code,
+                account.name,
+                account.type
+            ])
+
+            console.log(table.toString());
+        }
+        catch (e) {
+            s.stop();
+            getError(e)
+        }
     })
